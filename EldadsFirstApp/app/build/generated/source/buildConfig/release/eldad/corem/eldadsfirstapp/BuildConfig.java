d267a12d3bb5e655af7994f5b24d6ddc437e7f78
Eldad Corem <eldad.corem@gmail.com>
2016-11-02 17:51:28 +0200

    More ignored files

---
+++ EldadsFirstApp/app/build/generated/source/buildConfig/release/eldad/corem/eldadsfirstapp/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package eldad.corem.eldadsfirstapp;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String APPLICATION_ID = "eldad.corem.eldadsfirstapp";
+  public static final String BUILD_TYPE = "release";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
